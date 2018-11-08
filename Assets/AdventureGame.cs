ed2fc0c6c348652121c435e857a44bf1e72275f6
andjunior89 <andjunior89@gmail.com>
2018-11-08 15:49:34 -0300

    Alteração

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState( );
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates( );
+    }
+}
